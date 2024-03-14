515d10c277a2f32f2ab8bb3b0872075d54560aab
taha <163427680+tahatehrani7@users.noreply.github.com>
2024-03-14 18:05:14 +0330

    aval number
    In this code, a number is taken from the user and he is told whether it is prime or not
    In this code, a number is taken from the user and she is told whether it is prime or not
    در این کد از کاربر یک عدد گرفته شده به او گفته می شود که عدد اول است یا نه

---
+++ aval number.py
@@ -0,0 +1,9 @@
+def primenumber (n):
+    for i in range(2 , n):
+        if n % i == 0:
+            return "not aval"
+    else:
+        return  "aval"    
+a = int (input("enter namber: "))
+n = a
+print(primenumber(a))
